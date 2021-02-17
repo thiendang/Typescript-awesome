559777b65912f58083fe8a85b54ed5f4a28cdd3a
thiendang <thiendh310@gmail.com>
2021-02-17 22:55:19 +0700

    object type

---
+++ object.js
@@ -0,0 +1,5 @@
+var person = {
+    name: 'Maximilian',
+    age: 30
+};
+console.log(person.name);
---
+++ object.ts
@@ -0,0 +1,11 @@
+type personType = {
+  name: string;
+  age: number
+}
+
+const person: personType = {
+  name: 'Maximilian',
+  age: 30
+}
+
+console.log(person.name)
