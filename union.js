87c826ec092e2ef3f72e5fd8c54174ff6b1d981a
thiendang <thiendh310@gmail.com>
2021-02-20 16:19:14 +0700

    union type

---
+++ union.js
@@ -0,0 +1,14 @@
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combineAges = combine(5, 20);
+console.log(combineAges);
+var combineNames = combine('Max', 'Anna');
+console.log(combineNames);
